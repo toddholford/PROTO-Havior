10c2c8466ea384264413b439ec3981fea763e408
Todd Holford <toddholford1996@gmail.com>
2020-11-23 18:33:42 -0600

    added some stuff

---
+++ PROTO-Havior/Assets/Scripts/Character.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class Character : MonoBehaviour
+{
+    private static int attack = 6;
+    private static int defense = 5;
+    private static int speed = 4;
+    private static int health = 5;
+    private static int recovery = 4;
+    private string fighterType = "Swordsman";
+    private List<int>[,,,,] playerStats = new List<int>[attack, defense, speed, health, recovery];
+
+    private void mainMethod()
+    {
+        
+    }
+
+}
